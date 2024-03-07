55f62b874355d21e10ac2d2d388591ebe0471cf0
Antonlvanov <156616115+Antonlvanov@users.noreply.github.com>
2024-03-07 11:47:25 +0200

    Create tabeli loomine.sql

---
+++ tabeli loomine.sql
@@ -0,0 +1,30 @@
+-- tabeli toiduaine loomine
+CREATE TABLE toiduaine(
+    toiduaine_id int PRIMARY KEY AUTO_INCREMENT,
+    toiduaine_nimi varchar(100));
+
+-- tabeli yhik loomine
+CREATE TABLE yhik(	
+    yhik_id int PRIMARY KEY AUTO_INCREMENT,
+    yhim_nimi varchar(100));
+
+-- tabeli koostis loomine
+CREATE TABLE koostis(
+    koostis_id int PRIMARY KEY AUTO_INCREMENT,
+    kogus int,
+    retsept_retsept_id int,
+    toiduaine_id int,
+    yhik_id int,
+    FOREIGN KEY (toiduaine_id) REFERENCES toiduaine(toiduaine_id),
+    FOREIGN KEY (yhik_id) REFERENCES yhik(yhik_id),
+   );
+
+CREATE TABLE kasutaja(
+    kesutaja_id int PRIMARY KEY AUTO_INCREMENT,
+    eesnimi varchar(50),
+    perenimi varchar(50),
+    email varchar(150));
+
+CREATE TABLE kategooria(
+    kategooria_id int PRIMARY KEY AUTO_INCREMENT,
+    kesutaja_nimi varchar(50));
